e96e0a905b9f99360f58fca2630d275fafdf65a0
Davi Bastos <davi@easycomtec.com>
2022-04-01 23:32:00 -0300

    Add project files.

---
+++ AccManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AccManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
